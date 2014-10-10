1ce76c11263aad284129745c10136b24ae09dbb4
Elliot <e.salisbury@ecs.soton.ac.uk>
2014-10-10 12:07:17 +0100

    preview page now has instructions too

--- Retainer/index.php
+++ Retainer/index.php
@@ -15,7 +15,7 @@
 
 <script type="text/javascript" language="javascript"> 
 	// point to given instructions page, or default if no param is found
-	var task = gup('instructions') ? gup('instructions') : "instructions.php";
+	var task = gup('instructions') ? gup('instructions') : "instructions.php?task=" + gup("task");
 	$('.myIframe').attr('src', task);
 	
 	$('.myIframe').css('height', $(window).height()+'px');
